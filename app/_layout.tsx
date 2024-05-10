601dac36db71286988e9d934f128d0f66873a530
Xummp <luis.mulatoo@gmail.com>
2024-05-10 00:16:07 -0300

    upload projet

---
+++ app/_layout.tsx
@@ -0,0 +1,79 @@
+import { useFonts } from 'expo-font';
+import { Slot, SplashScreen, useRouter, useSegments } from 'expo-router';
+import { useEffect } from 'react';
+import { ClerkProvider, useAuth } from '@clerk/clerk-expo';
+import * as SecureStore from 'expo-secure-store';
+
+const CLERK_PUBLISHABLE_KEY = "pk_test_c3BsZW5kaWQtbW9zcXVpdG8tOTAuY2xlcmsuYWNjb3VudHMuZGV2JA";
+// Cache the Clerk JWT
+const tokenCache = {
+  async getToken(key: string) {
+    try {
+      return SecureStore.getItemAsync(key);
+    } catch (err) {
+      return null;
+    }
+  },
+  async saveToken(key: string, value: string) {
+    try {
+      return SecureStore.setItemAsync(key, value);
+    } catch (err) {
+      return;
+    }
+  },
+};
+
+const InitialLayout = () => {
+  const { isLoaded, isSignedIn } = useAuth();
+  const segments = useSegments();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!isLoaded) return;
+
+    const inTabsGroup = segments[0] === '(auth)';
+
+    console.log('User changed: ', isSignedIn);
+
+    /*if (isSignedIn && !inTabsGroup) {
+      router.replace('/home');
+    } else if (!isSignedIn) {
+      router.replace('/login');
+    }*/
+    router.replace('/login');
+  }, [isSignedIn]);
+
+  return <Slot />;
+};
+
+// Prevent the splash screen from auto-hiding before asset loading is complete.
+SplashScreen.preventAutoHideAsync();
+
+export default function RootLayout() {
+  const [loaded, error] = useFonts({
+    mon: require('../assets/fonts/Montserrat-Regular.ttf'),
+    'mon-sb': require('../assets/fonts/Montserrat-SemiBold.ttf'),
+    'mon-b': require('../assets/fonts/Montserrat-Bold.ttf'),
+  });
+
+  // Expo Router uses Error Boundaries to catch errors in the navigation tree.
+  useEffect(() => {
+    if (error) throw error;
+  }, [error]);
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+
+  return (
+    <ClerkProvider publishableKey={CLERK_PUBLISHABLE_KEY!} tokenCache={tokenCache}>
+      <InitialLayout />
+    </ClerkProvider>
+  );
+}
